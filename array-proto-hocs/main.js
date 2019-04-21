ad31dfd93cdbae42815ef8e199be054846c566ce
Denis Kirilenko <kirilenko.dv@gmail.com>
2019-04-21 17:55:55 +0300

    commit array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,35 +1,57 @@
 "use strict"
 
-let results = [{}];
+function compareArrays(arr1,arr2){
 
-function compareArrays( arr1, arr2 ) {
-	//let response = true;
+  if(!arr1  || !arr2) return
 
-	if (arr1.length != arr2.length) {
-		
-		return false;
+  let result;
 
-	} else {
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] != arr2[i]) {
-				return false;
-			}
-		}
-	}
+  arr1.forEach((e1,i)=>arr2.forEach(e2=>{
 
-	
-	console.log(arr1, arr2);
-	return true;
-}
+      if(e1.length > 1 && e2.length){
+        result = compare(e1,e2);
+      }else if(e1 !== e2 ){
+        result = false
+      }else{
+        result = true
+      }
+    })
+  )
 
-function execute(func) {
-
+  return result
 
 }
-execute(compareArrays([8, 9], [8, 9]));
-console.log(execute(compareArrays([8, 9], [8, 9])));
 
-function memoize(fn, limit) {
+compareArrays([8, 9], [8, 9]);
+console.log(compareArrays([8, 9], [8, 9]));
+const add = (a, b) => (a + b);
+console.log(add(3, 4));
 
+const memoize = (fn) => {
+  let cache = [{}];
+  return (...args) => {
+    let n = args[0];
+    let y = args[1];
+    for (let key in cache) {
+      let result = compareArrays(cache[key].args, [n, y])
+      if (result) {
+        console.log('Memory');
+        return cache[key].result
+      }
+    }
+      console.log('Calculating result');
+      let result = fn(n, y);
+      cache.push({args: [n, y], result: result})
+      //cache[n] = result;
+      return result;
+
+  }
 }
+
+const memoizedAdd = memoize(add);
+console.log(memoizedAdd(3, 4));  
+console.log(memoizedAdd(3, 4));  
+
+
+
 
