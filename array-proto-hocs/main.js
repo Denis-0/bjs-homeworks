4914e9e3538a0ad2962b1fa4837620162fbc8507
Denis Kirilenko <kirilenko.dv@gmail.com>
2019-04-21 20:55:48 +0300

    исправил массив args

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,7 +1,6 @@
 "use strict"
 
 function compareArrays(arr1,arr2){
-
   if(!arr1  || !arr2) return
 
   let result;
@@ -24,25 +23,23 @@ function compareArrays(arr1,arr2){
 
 compareArrays([8, 9], [8, 9]);
 console.log(compareArrays([8, 9], [8, 9]));
-const add = (a, b) => (a + b);
+const add = (x, y) => (x + y);
 console.log(add(3, 4));
 
 const memoize = (fn) => {
   let cache = [{}];
   return (...args) => {
-    let n = args[0];
-    let y = args[1];
     for (let key in cache) {
-      let result = compareArrays(cache[key].args, [n, y])
+      let result = compareArrays(cache[key].args, args)
       if (result) {
-        console.log('Memory');
+        console.log('Fetching from cache');
         return cache[key].result
       }
     }
       console.log('Calculating result');
-      let result = fn(n, y);
-      cache.push({args: [n, y], result: result})
-      //cache[n] = result;
+      let result = fn(...args);
+      cache.push({args: args, result: result})
+      
       return result;
 
   }
@@ -51,6 +48,8 @@ const memoize = (fn) => {
 const memoizedAdd = memoize(add);
 console.log(memoizedAdd(3, 4));  
 console.log(memoizedAdd(3, 4));  
+console.log(memoizedAdd(3, 6));
+console.log(memoizedAdd(3, 6));
 
 
 
