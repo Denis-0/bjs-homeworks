6f259b47fe57f0eec91a7aee3e3c67abf25af9f3
Denis Kirilenko <kirilenko.dv@gmail.com>
2019-04-23 23:16:08 +0300

    добавил every()

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,25 +1,54 @@
 "use strict"
 
-function compareArrays(arr1,arr2){
-  if(!arr1  || !arr2) return
-
-  let result;
-
-  arr1.forEach((e1,i)=>arr2.forEach(e2=>{
-
-      if(e1.length > 1 && e2.length){
-        result = compare(e1,e2);
-      }else if(e1 !== e2 ){
-        result = false
-      }else{
-        result = true
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let set = {};
+  arr1.forEach((i) => {
+    if (set[i] !== undefined) {
+      set[i]++;
+    } else {
+      set[i] = 1;
+    }
+  });
+  let difference = arr2.every((i) => {
+    if (set[i] === undefined) {
+      return false;
+    } else {
+      set[i]--;
+      if (set[i] === 0) {
+        delete set[i];
       }
-    })
-  )
-
-  return result
-
+      return true;
+    }
+  });
+  return Object.keys(set) == 0 && difference;
 }
+
+
+
+
+// function compareArrays(arr1,arr2){
+//   if(!arr1  || !arr2) return
+
+//   let result;
+
+//   arr1.every((e1,i)=>arr2.every(e2=> {
+
+//       if(e1.length > 1 && e2.length){
+//         result = compare(e1,e2);
+//       }else if(e1 !== e2 ){
+//         result = false
+//       }else{
+//         result = true
+//       }
+//     })
+//   )
+
+//   return result
+
+// }
 
 compareArrays([8, 9], [8, 9]);
 console.log(compareArrays([8, 9], [8, 9]));
@@ -27,18 +56,18 @@ const add = (x, y) => (x + y);
 console.log(add(3, 4));
 
 const memoize = (fn) => {
-  let cache = [{}];
+  let memory = [];
   return (...args) => {
-    for (let key in cache) {
-      let result = compareArrays(cache[key].args, args)
+    for (let key in memory) {
+      let result = compareArrays(memory[key].args, args)
       if (result) {
-        console.log('Fetching from cache');
-        return cache[key].result
+        console.log('Fetching from memory');
+        return memory[key].result
       }
     }
       console.log('Calculating result');
       let result = fn(...args);
-      cache.push({args: args, result: result})
+      memory.push({args: args, result: result})
       
       return result;
 
